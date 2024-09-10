c2fa45699e99fc0de3ed4af18a0b11a79c83f627
kwon6504 <152842092+kwon6504@users.noreply.github.com>
2024-09-10 10:56:43 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.03 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.java
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.java
@@ -6,12 +6,6 @@ class Solution {
         for(int i = 0; i < num_list.length; i++){
             answer[i/n][i%n] = num_list[i];
         }
-        for(int i = 0; i<rows; i++){
-            for(int j = 0; j<n; j++){
-                System.out.print(answer[i][j] + " ");
-            }
-            System.out.println();
-        }
         return answer;
     }
 }
